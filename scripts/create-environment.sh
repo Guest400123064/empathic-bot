130f07635cdd6becc1254fd0c43e0783593e266a
guest400123064 <lgfz1003@icloud.com>
2022-11-04 22:26:14 -0400

    re-organize env creation scripts

--- scripts/create-environment.sh
+++ scripts/create-environment.sh
@@ -8,26 +8,24 @@ conda deactivate
 conda create --force -n parlai python=3.9
 conda activate parlai
 
-# Install a ParlAI dependency that is not documented
-#   If the following command fails, please run:
-#       `sudo apt install libxml2-dev`
-conda install -c conda-forge cudatoolkit-dev
-
 # For vscode notebook env
 pip install ipykernel ipywidgets
 
 # Install openai (for gpt3 prompting)
 pip install openai
 
+# Install transformers
+pip install transformers
+
 # Install torch
-conda install pytorch torchvision torchaudio cudatoolkit=11.6 -c pytorch -c conda-forge
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 
 # Install ParlAI and Mephisto from source
 cd packages
 
-# Install Mephisto
-cd Mephisto
-pip install -e .
+# Install Search Engine dependencies
+cd SearchEngine
+pip install -r ./requirements.txt
 cd ..
 
 # Install ParlAI
@@ -43,13 +41,14 @@ cd ..
 #       + hydra-core<1.1,>=1.0.7 (but you have hydra-core 1.2.0)
 #       + omegaconf<2.1 (but you have omegaconf 2.2.3)
 #   - however, the code seems to be running at this point
-cd Fairseq
-pip install -e .
-cd ..
+pip install fairseq
+# cd Fairseq
+# pip install -e .
+# cd ..
 
-# Install Search Engine dependencies
-cd SearchEngine
-pip install -r ./requirements.txt
+# Install Mephisto
+cd Mephisto
+pip install -e .
 cd ..
 
 # Wrap-up
