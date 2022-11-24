7347c5ad914b4b8dd3f783571d484a8c02e1b137
guest400123064 <lgfz1003@icloud.com>
2022-11-24 00:31:22 -0500

    .

--- scripts/setup-mephisto.sh
+++ scripts/setup-mephisto.sh
@@ -2,9 +2,10 @@
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda deactivate
-
 conda activate parlai
 
 # Setup mephisto data folder
+mkdir -p ./data/mephisto-data
+
 mephisto config core.main_data_directory ./data/mephisto-data
 mephisto check
