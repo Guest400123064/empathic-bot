b0335ed3ba0d48eae35a01f999c8784bc4b01130
guest400123064 <lgfz1003@icloud.com>
2022-12-10 03:01:19 -0500

    .new client

--- apps/browser_client/_gui.py
+++ apps/browser_client/_gui.py
@@ -8,7 +8,7 @@ This file copies static HTML from parlai.interactive_web.py for a simple
   chatbot client frontend. However, the reset functionality is removed.
 """
 
-END_MESSAGE  = ("Thank you for chatting with me! Chat history saved at 'chat/< YOUR ID >-< DATETIME >.json'. "
+END_MESSAGE  = ("Chat history saved at < chat/< YOUR ID >-< DATETIME >.json >. "
                 "Please upload the chat history to the google drive at < https://drive.google.com/drive/folders/1UBtqF_PXoOwkOSvGZrjiBdy_6utJCSrm?usp=sharing >. "
                 "And please finish the survey at < https://nyu.qualtrics.com/jfe/form/SV_26jZ2beozdQrcHk >.")
 STYLE_SHEET  = "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css"
