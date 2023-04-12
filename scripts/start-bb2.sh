87fa8b66138344c09b6862c7e7d4b19f4f335e00
guest400123064 <lgfz1003@icloud.com>
2023-04-12 19:56:27 -0400

    new start script

--- scripts/start-bb2.sh
+++ scripts/start-bb2.sh
@@ -4,13 +4,20 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda deactivate
 conda activate parlai
 
+# Save current directory
+export PARLAI_EMPATHIC_BOT_DIR=$(pwd)
+
 # Start search engine for blenderbot2
 #   - assuming the <parlai> environment is installed
 #   - will serve at <localhost:5000>
-python ./packages/SearchEngine/main.py serve \
+cd ./packages/SearchEngine/
+python main.py serve \
     --host localhost:5000 \
     --search_engine="CustomDoc" &
+cd $PARLAI_EMPATHIC_BOT_DIR
 
 # Start BlenderBot2 in background
-python ./apps/terminal_server/main.py \
+cd ./apps/terminal_server/
+python main.py \
     --config-path ./apps/terminal_server/config/config_bb2.yml &
+cd $PARLAI_EMPATHIC_BOT_DIR
